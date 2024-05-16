43dd123d679c7e238c5b8b78e3fcf0a18764c7c3
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-05-16 15:13:32 -0300

    Criando stories dos componentes

---
+++ libs/storybook-host/src/input.component.stories.ts
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { InputComponent } from '../../input/src/lib/input/input.component';
+
+const meta: Meta<InputComponent> = {
+  component: InputComponent,
+  title: 'InputComponent',
+};
+export default meta;
+type Story = StoryObj<InputComponent>;
+
+export const Primary: Story = {
+  args: {
+    multilinha: false,
+  },
+};
