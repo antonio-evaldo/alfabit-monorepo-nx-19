38df2359d54f7d4e2365c37788849d19a5fdf624
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-05-22 11:19:55 -0300

    fix(button): corrige bug

--- libs/button/src/lib/button/button.component.ts
+++ libs/button/src/lib/button/button.component.ts
@@ -14,7 +14,7 @@ type temas = 'azul' | 'violeta';
 export class ButtonComponent {
 
   /**
-   * Texto atualizado do botão.
+   * Texto do botão.
    */
   @Input() texto = '';
   @Input() desabilitado = false;
