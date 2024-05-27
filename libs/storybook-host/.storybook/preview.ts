993a0b702633c07d87f27e9888f9f18e79c78105
Nayanne Batista <nayannelopes.bd@gmail.com>
2024-05-27 14:42:32 -0300

    Adicionando ordenação alfabética no storybook

--- libs/storybook-host/.storybook/preview.ts
+++ libs/storybook-host/.storybook/preview.ts
@@ -1,3 +1,19 @@
 import { setCompodocJson } from '@storybook/addon-docs/angular';
 import docJson from '../documentation.json';
+import { Preview } from '@storybook/angular';
 setCompodocJson(docJson);
+
+const preview: Preview = {
+  parameters: {
+    actions: {
+      argTypesRegex: "^on [A-Z].*"
+    },
+    options: {
+      storySort: {
+      method: "alphabetical",
+      locales: "pt-BR",
+      },
+    },
+  },
+}
+export default preview;
