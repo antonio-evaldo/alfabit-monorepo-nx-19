c9a4513f534cf77d7242eea465d610ddcddaec52
Nayanne Batista <nayannelopes.bd@gmail.com>
2024-05-27 16:18:19 -0300

    Criando nova lib de tipografia

---
+++ libs/ui/typography/src/lib/typography/typography.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'org-teste-typography',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './typography.component.html',
+  styleUrl: './typography.component.css',
+})
+export class TypographyComponent {}
